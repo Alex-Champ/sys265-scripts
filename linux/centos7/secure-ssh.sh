49b08696a9de0a2fb87875d26ebafa10a62433b2
Alex-Champ <alexander.marvi@mymail.champlain.edu>
2019-02-27 23:29:28 -0500

    Script Finalized

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -1,8 +1,12 @@
 #SECURE-SSH.SH
 #AUTHOR ALEX-CHAMP
+#!/bin/sh
 #Creates a new ssh user called $1
+sudo useradd $1
+sudo mkdir /home/$1/.ssh
 #Adds a public key to that users authorized keys file
+sudo cp ../public-keys/sys265.pub /home/$1/.ssh/authorized_keys
+sudo chmod 700 /home/$1/.ssh
+sudo chmod 600 /home/$1/.ssh/authorized_keys
 #Removes roots ability to ssh in
-
-
-echo "testing 123"
+sudo echo "PermitRootLogin No" | sudo tee -a /etc/ssh/sshd_config
